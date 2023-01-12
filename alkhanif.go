ba151a9854d78c8f9fdb6982a167594fb24d13fb
Azams <azams.net@gmail.com>
2023-01-12 14:19:35 +0800

    Update alkhanif.go

--- alkhanif.go
+++ alkhanif.go
@@ -1,4 +1,4 @@
-package alkhanif
+package tools
 
 import (
 	"crypto/md5"
@@ -49,14 +49,14 @@ func CompileRequest(data string, ssl bool) (targetUrl string, method string, bod
 	return targetUrl, method, body, req, httpversion
 }
 
-func isFileExists(filename string) bool {
+func IsFileExists(filename string) bool {
 	if _, err := os.Stat(filename); err == nil {
 		return true
 	}
 	return false
 }
 
-func isDirectory(path string) (bool, error) {
+func IsDirectory(path string) (bool, error) {
 	fileInfo, err := os.Stat(path)
 	if err != nil {
 		return false, err
@@ -64,16 +64,16 @@ func isDirectory(path string) (bool, error) {
 	return fileInfo.IsDir(), err
 }
 
-func getFileExtension(filename string) string {
+func GetFileExtension(filename string) string {
 	splitted := strings.Split(filename, ".")
 	return splitted[len(splitted)-1]
 }
 
-func base64encode(source string) string {
+func Base64encode(source string) string {
 	return b64.StdEncoding.EncodeToString([]byte(source))
 }
 
-func base64decode(source string) string {
+func Base64decode(source string) string {
 	dec, err := b64.StdEncoding.DecodeString(source)
 	if err != nil {
 		return ""
@@ -81,7 +81,7 @@ func base64decode(source string) string {
 	return string(dec)
 }
 
-func grabAllString(source string, regex string) [][]string {
+func GrabAllString(source string, regex string) [][]string {
 	r, err := regexp.Compile(regex)
 	if err != nil {
 		return [][]string{}
@@ -89,7 +89,7 @@ func grabAllString(source string, regex string) [][]string {
 	return r.FindAllStringSubmatch(source, -1)
 }
 
-func grabString(source string, regex string) []string {
+func GrabString(source string, regex string) []string {
 	r, err := regexp.Compile(regex)
 	if err != nil {
 		return []string{}
@@ -97,7 +97,7 @@ func grabString(source string, regex string) []string {
 	return r.FindStringSubmatch(source)
 }
 
-func getFilePermission(filename string) string {
+func GetFilePermission(filename string) string {
 	fileInfo, err := os.Stat(filename)
 	if err != nil {
 		return ""
@@ -105,7 +105,7 @@ func getFilePermission(filename string) string {
 	return fmt.Sprintf("%v", fileInfo.Mode())
 }
 
-func getFileSize(filename string) string {
+func GetFileSize(filename string) string {
 	fileinfo, err := os.Stat(filename)
 	if err != nil {
 		return ""
@@ -113,7 +113,7 @@ func getFileSize(filename string) string {
 	return strconv.FormatInt(fileinfo.Size(), 10)
 }
 
-func hasher(origin string, mode string) string {
+func Hasher(origin string, mode string) string {
 	if mode == "md5" {
 		return fmt.Sprintf("%v", md5.Sum([]byte(origin)))
 	} else if mode == "sha1" {
@@ -125,4 +125,16 @@ func hasher(origin string, mode string) string {
 	} else {
 		return "Encryption does not supported"
 	}
+}
+
+func VisitURL(link string) (string, error) {
+	res, err := http.Get(link)
+	if err != nil {
+		return "", err
+	}
+	result, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return "", err
+	}
+	return string(result), nil
 }
